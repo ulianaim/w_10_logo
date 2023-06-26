1b28c9881b6143377874c5e5b794636d68b11d92
ulianim <uliana_usa@yahoo.com>
2023-06-26 18:33:41 -0400

    New changes to the code

--- Libraries/shapes.js
+++ Libraries/shapes.js
@@ -1,28 +1,29 @@
 class Shapes {
     constructor() {
-      this.color = ""
+        this.color = ""
     }
-  
+
     setColor(color) {
         this.color = color;
     }
-  }
- 
-  class Triangle extends Shapes {
-     render() {
-        `<polygon points="250,60 100,400 400,400" fill="${this.color}"/>`
-     }
-    
-  }
-  class Circle extends Shapes {
+}
+
+class Triangle extends Shapes {
     render() {
-       `<circle cx="150" cy="100" r="80" fill="${this.color}"/>`
+      return  `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}"/>`
+
+    }
+
+}
+class Circle extends Shapes {
+    render() {
+       return `<circle cx="150" cy="100" r="80" fill="${this.color}"/>`
     }
 }
 class Square extends Shapes {
     render() {
-       `<rect x="50" y="20" width="150" height="150" fill="${this.color}"/>`
+       return `<rect x="50" y="20" width="150" height="150" fill="${this.color}"/>`
     }
 }
 
-module.exports = {Triangle, Circle, Square}
+module.exports = { Triangle, Circle, Square }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Packages that neede for this app
 const inquirer = require('inquirer')
 const fs = require('fs');
-const {Triangle, Circle, Square} = require("./Libraries/shapes")
+const {Triangle, Circle, Square} = require("./Libraries/shapes.js")
 
 function generateSVGData(data) {
     let shape;
@@ -21,9 +21,16 @@ function generateSVGData(data) {
                 shape.setColor (data.shapeColor)
             break;
     }
-    return `<svg width="300" height="200">
+//     return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+//     <circle cx="150" cy="100" r="80" fill="${data.shapeColor}" />
+  
+//     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.nameColor}">${data.name}</text>
+  
+//   </svg>`
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     ${shape.render()}
-    <text x="0" y="0" fill="${data.nameColor}">${data.name}</text>
+    <text x="115" y="140" font-size="40" fill="${data.nameColor}">${data.name}</text>
   </svg>`;
 
 }
